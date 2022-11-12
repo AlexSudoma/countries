5a2207b8a6e4d54de0deb1a70cb32fabb9ac1050
AlexKaylinir <alexkaylinir@gmail.com>
2022-11-12 11:03:24 +0200

    new2

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 console.log(countries);
 var htmlString = '';
-for (var item = 0; item < countries.length; item++) {
-    var country = countries[item];
+for (var country of countries) {
     htmlString += `<tr>
     <td>${country.name.official}</td>
     <td>${country.region}</td>
     <td>${country.population}</td>
-    <td><img src="${country.flags.png}" class="col-10"></td>
+    <td class="text-center"><img src="${country.flags.png}" class="w-5-rem"></td>
 </tr>`
 }
 document.getElementById('countries').innerHTML = htmlString;
